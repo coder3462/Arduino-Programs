11d3d5c809564575a1dc5c4972012645a04635ec
coder3462 <rahuldshah3@gmail.com>
2017-06-22 17:05:18 -0400

    Add files via upload

---
+++ Project_6A.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+int x = 1; //variable
+int variable = 5;
+int multiplier = 5;
+
+for (x=1; x<=10; ++x)  //the for loop
+{ Serial.print(x); Serial.print(" x "); Serial.print(variable); Serial.print(" = ");Serial.println(x*multiplier); Serial.println(" ");}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Temp-Sensor.ino
@@ -0,0 +1,37 @@
+// Use analog input 0 to measure the temperature sensor's signal pin
+
+const int temperaturePin = 0;
+
+void setup()
+{  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+ 
+  float voltage, degreesC, degreesF;
+
+  voltage = getVoltage(temperaturePin);  //function to return the true voltage (0 to 5 volts) on the sensor
+ 
+  degreesC = (voltage - 0.5) * 100.0;         // Convert the voltage to degrees Celsius.
+ 
+  degreesF = degreesC * (9.0/5.0) + 32.0;     // Convert degrees Celsius to Fahrenheit.
+ 
+  //print the values of the voltage, degrees celcius, degrees fahrenheit
+
+  Serial.print("voltage: ");
+  Serial.print(voltage);
+  Serial.print("  deg C: ");
+  Serial.print(degreesC);
+  Serial.print("  deg F: ");
+  Serial.println(degreesF);
+   
+  delay(1000); // repeat once per second
+}
+float getVoltage(int pin) //user-defined function to return the true voltage (0 to 5 volts) on the temperature sensor
+{
+   return (analogRead(pin) * 0.004882814); //read the value of the temperature sensor and convert it to a true voltage reading
+ }
+
+
